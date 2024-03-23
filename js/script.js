d5479b36e800eb17b54a770cd68a802ddea7b3b1
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-03-23 23:59:30 +0300

    commit
    commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,54 @@ const global = {
 
 // console.log(global.currentPage);
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular"); // if you put {} around results you can get arrays seperated as in {}s
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+          : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+          />`
+      }
+      
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>
+      `;
+  });
+
+  document.querySelector("#popular-movies").appendChild(div);
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "3f6aee75fcb14b83b975444f5a9b4513";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight Active Link
 function highlightActiveLink() {
   const link = document.querySelectorAll(".nav-link");
@@ -28,6 +76,7 @@ function init() {
     case "/":
     case "/index.html":
       console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
