aea48fcdf605f944f51015b137dce6ecb3ca4186
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-03-26 21:43:14 +0300

    commit
    commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@
 
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+
+  api: {
+    apiKey: "3f6aee75fcb14b83b975444f5a9b4513",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 // console.log(global.currentPage);
@@ -217,6 +229,126 @@ async function displayShowDetails() {
   document.querySelector("#show-details").appendChild(div);
 }
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString); //takes url and uses methods on it
+  // console.log(queryString); // ?type=movie&search-term='X' - X is what is searched
+  // console.log(urlParams.get("search-term")); // X
+  // console.log(urlParams.get("type")); // movie
+
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    // @todo - make request and display results
+    const { results, total_pages, page, total_results } = await searchAPIData();
+    console.log(results);
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    if (results.length === 0) {
+      showAlert("No results found", "error");
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
+  } else {
+    showAlert("Please enter a search term", "error");
+  }
+}
+
+function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
+
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="${global.search.type}-details.html?id=${result.id}">
+      ${
+        result.poster_path
+          ? `<img
+            src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+            class="card-img-top"
+            alt="${global.search.type === "movie" ? result.title : result.name}"
+          />`
+          : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+          />`
+      }
+      
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${
+          global.search.type === "movie" ? result.title : result.name
+        }</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${
+            global.search.type === "movie"
+              ? result.release_date
+              : result.first_air_date
+          }</small>
+        </p>
+      </div>
+      `;
+
+    document.querySelector("#search-results-heading").innerHTML = `
+          <h2>${results.length} of ${global.search.totalResults} results for ${global.search.term}</h2>
+    `;
+
+    document.querySelector("#search-results").appendChild(div);
+  });
+
+  displayPagination();
+}
+
+// Create & Display Pagination for Search
+function displayPagination() {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector("#pagination").appendChild(div);
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+
+  // Next Page
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev Page
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results } = await searchAPIData();
+    displaySearchResults(results);
+  });
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
@@ -301,13 +433,33 @@ function displayBackgroundImage(type, backgroundPath) {
 async function fetchAPIData(endpoint) {
   // Register your key at https://www.themoviedb.org/settings/api and enter here
   // Only use this for development or very small projects. You should store your key and make requests from a server
-  const API_KEY = "3f6aee75fcb14b83b975444f5a9b4513";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+// Fetch data from TMDB API
+async function searchAPIData() {
+  // Register your key at https://www.themoviedb.org/settings/api and enter here
+  // Only use this for development or very small projects. You should store your key and make requests from a server
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
@@ -340,6 +492,16 @@ function highlightActiveLink() {
   }
 }
 
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 function addCommasToNumber(number) {
   // regular expression
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -369,6 +531,7 @@ function init() {
       break;
     case "/search.html":
       console.log("Search");
+      search();
       break;
   }
 }
